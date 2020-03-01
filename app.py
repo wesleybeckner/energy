c995183fa8364c6f067ff6cfeb763ee405747cb7
Wesley Beckner <wesleybeckner@gmail.com>
2020-02-29 19:31:08 -0600

    changed bar plot to respond to dropdown. auto responds to axis duplication by moving sites from axis to legend. next will add time frame to barplot title, units to metric name

--- app.py
+++ app.py
@@ -52,7 +52,11 @@ server = app.server
 #     )
 #     return fig
 
-def make_time_plot(clickData=None, selectedData=None, dropdown=None):
+def make_time_plot(clickData=None,
+                   selectedData=None,
+                   dropdown=None,
+                   sample='Year',
+                   plot_type='Scatter'):
     if dropdown != None:
         items = dropdown
     else:
@@ -76,49 +80,102 @@ def make_time_plot(clickData=None, selectedData=None, dropdown=None):
     if len(units) == 1:
         titles = ["{}".format(df.index.get_level_values(1).unique()[0])]
         if '/' not in units: # sum Item values if no '/' otherwise take weight average
-            for index in df.index.get_level_values(0).unique():
+            if sample == 'Year':
+                df2 = df.T.resample('Y').sum().T
+            else:
+                df2 = df
+            for index in df2.index.get_level_values(0).unique():
+                # if plot_type == 'Scatter':
+                #     fig.add_trace(
+                #         go.Scatter(
+                #         name=index,
+                #         y=df2.loc[index].sum(),
+                #         x=df2.columns,
+                #         ),
+                #         )
+                # else:
                 fig.add_trace(
-                go.Scatter(
-                name=index,
-                y=df.loc[index].sum(),
-                x=df.columns,
-                ),
-            )
+                    go.Bar(
+                    name=index,
+                    y=df2.loc[index].sum(),
+                    x=df2.columns,
+                    ),
+                    )
         else:
-            for index in df.index.get_level_values(0).unique():
+            if sample == 'Year':
+                df2 = df.T.resample('Y').mean().T
+            else:
+                df2 = df
+            for index in df2.index.get_level_values(0).unique():
+                # if plot_type == 'Scatter':
+                #     fig.add_trace(
+                #     go.Scatter(
+                #     name=index,
+                #     y=df2.loc[index].mean(),
+                #     x=df2.columns,
+                #     ),
+                #     )
+                # else:
                 fig.add_trace(
-                go.Scatter(
+                go.Bar(
                 name=index,
-                y=df.loc[index].mean(),
-                x=df.columns,
+                y=df2.loc[index].mean(),
+                x=df2.columns,
                 ),
-            )
+                )
     else:
-
         titles = []
         for index2, unit in enumerate(units):
             df2 = df.loc[(df.index.get_level_values(1) == unit)]
             titles.append("{}".format(df2.index.get_level_values(1).unique()[0]))
             if '/' not in unit: # sum Item values if no '/' otherwise take weight average
+                if sample == 'Year':
+                    df2 = df2.T.resample('Y').sum().T
+                else:
+                    df2 = df2
                 for index in df2.index.get_level_values(0).unique():
-                    fig.add_trace(
-                    go.Scatter(
-                    name="{} ({})".format(index,unit),
-                    y=df2.loc[index].sum(),
-                    x=df2.columns,
-                    yaxis="y{}".format(index2+1)
-                    ),
-                )
+                    if plot_type == 'Scatter':
+                        fig.add_trace(
+                        go.Scatter(
+                        name="{} ({})".format(index,unit),
+                        y=df2.loc[index].sum(),
+                        x=df2.columns,
+                        yaxis="y{}".format(index2+1)
+                        ),
+                        )
+                    else:
+                        fig.add_trace(
+                        go.Bar(
+                        name="{} ({})".format(index,unit),
+                        y=df2.loc[index].sum(),
+                        x=df2.columns,
+                        yaxis="y{}".format(index2+1)
+                        ),
+                        )
             else:
+                if sample == 'Year':
+                    df2 = df2.T.resample('Y').mean().T
+                else:
+                    df2 = df2
                 for index in df2.index.get_level_values(0).unique():
-                    fig.add_trace(
-                    go.Scatter(
-                    name="{} ({})".format(index,unit),
-                    y=df2.loc[index].mean(),
-                    x=df2.columns,
-                    yaxis="y{}".format(index2+1)
-                    ),
-                )
+                    if plot_type == 'Scatter':
+                        fig.add_trace(
+                        go.Scatter(
+                        name="{} ({})".format(index,unit),
+                        y=df2.loc[index].mean(),
+                        x=df2.columns,
+                        yaxis="y{}".format(index2+1)
+                        ),
+                        )
+                    else:
+                        fig.add_trace(
+                        go.Bar(
+                        name="{} ({})".format(index,unit),
+                        y=df2.loc[index].mean(),
+                        x=df2.columns,
+                        yaxis="y{}".format(index2+1)
+                        ),
+                        )
 
     # if line > 1 view as boxplot
     # view as button: rollup 1 M, 1 Y, All
@@ -164,7 +221,7 @@ def make_time_plot(clickData=None, selectedData=None, dropdown=None):
                    'title': {'text': '{}'.format(titles[i-1])}}
         }
         )
-    fig.update_layout(xaxis_rangeslider_visible=False)
+    fig.update_layout(xaxis_rangeslider_visible=True)
 
     if len(items) == 1:
         title="TIMESERIES; {}; {}".format(items[0], ", ".join(line.values))
@@ -172,6 +229,7 @@ def make_time_plot(clickData=None, selectedData=None, dropdown=None):
         title="TIMESERIES; {}".format(", ".join(line.values))
     fig.update_layout(dict(
         title=title,
+        barmode="stack",
         xaxis={'rangeselector': {'buttons': list([{'count': 1, 'label': '1M', 'step': 'month', 'stepmode': 'backward'},
                                                   {'count': 6, 'label': '6M', 'step': 'month', 'stepmode': 'backward'},
                                                   {'step': 'all'}])}},
@@ -180,7 +238,9 @@ def make_time_plot(clickData=None, selectedData=None, dropdown=None):
     return fig
 
 
-def make_bar_plot(relayoutData=None):
+def make_bar_plot(relayoutData=None,
+                  dropdown=None,):
+    # time filter
     data_filt = data
     start_str = end_str = None
     if relayoutData != None:
@@ -201,47 +261,132 @@ def make_bar_plot(relayoutData=None):
             data_filt = data[cols]
         else:
             data_filt = data
-    # create usage df
-    df = data_filt.mean(axis=1).reorder_levels([2,1,0])
-    df = pd.DataFrame(df).unstack()
-    df = df.reorder_levels([1,0]).loc['GJ'][0].dropna(axis=1)
-    df['Totals'] = df.sum(axis=1)
-    df = df.sort_values(by='Totals')
 
-    # create CO2/MT df
-    df2 = pd.DataFrame(data_filt.mean(axis=1)).loc['CO2/production']
-    df2 = df2.loc['mt/mt']
-    df2.columns = ['CO2/production']
-    df2 = df2.reindex(df.index)
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-    for col in df.columns[:-1]:
-        fig.add_trace(
-        go.Bar(
-        name=col,
-        y=df[col],
-        x=df.index,
+    # item filter
+    if dropdown != None:
+        items = dropdown
+    else:
+        items = ['Electricity', 'Nat. Gas', 'Steam usage']
+        items = items + ['CO2/production']# + ['Energy Intensity']
+    #     items = ['Energy Intensity']
+
+    # if type(line) == str:
+    #     line = pd.Series(line)
+
+    df = data_filt.loc[(data_filt.index.get_level_values(0).isin(items))]
+
+    units = df.index.get_level_values(1).unique() # detect number of different unit types; compute axes
+
+    fig = go.Figure()
+    colors = [
+        '#1f77b4',  # muted blue
+        '#ff7f0e',  # safety orange
+        '#2ca02c',  # cooked asparagus green
+        '#d62728',  # brick red
+        '#9467bd',  # muted purple
+        '#8c564b',  # chestnut brown
+        '#e377c2',  # raspberry yogurt pink
+        '#7f7f7f',  # middle gray
+        '#bcbd22',  # curry yellow-green
+        '#17becf'   # blue-teal
+    ]
+    if len(units) == 1:
+        titles = ["{}".format(df.index.get_level_values(1).unique()[0])]
+        if '/' not in units: # sum Item values if no '/' otherwise take weight average
+            df2 = df.reorder_levels([0,2,1])
+            for index in df2.index.get_level_values(0).unique():
+                fig.add_trace(
+                    go.Bar(
+                    name=index,
+                    y=df2.loc[index].sum(axis=1),
+                    x=df2.loc[index].index.get_level_values(0),
+                    ),
+                    )
+        else:
+            for index in df2.index.get_level_values(0).unique():
+                fig.add_trace(
+                    go.Bar(
+                    name=index,
+                    y=df2.loc[index].mean(axis=1),
+                    x=df2.loc[index].index.get_level_values(0),
+                    ),
+                    )
+    else:
+        titles = []
+        for index2, unit in enumerate(units):
+            df2 = df.loc[(df.index.get_level_values(1) == unit)]
+            df2 = df2.reorder_levels([2,0,1])
+            titles.append("{}".format(df2.index.get_level_values(2).unique()[0]))
+            for ind, index in enumerate(df2.index.get_level_values(0).unique()):
+                if index2 > 0:
+                    showlegend=False
+                else:
+                    showlegend=True
+                yaxis="y{}".format(index2+1)
+                if '/' not in unit: # sum Item values if no '/' otherwise take weight average
+                    y=df2.loc[index].sum(axis=1)
+                else:
+                    y=df2.loc[index].mean(axis=1)
+                fig.add_trace(
+                go.Bar(
+                name="{}".format(index),
+                y=y,
+                x=df2.loc[index].index.get_level_values(0),
+                legendgroup=index,
+                yaxis=yaxis,
+                marker_color=colors[ind],
+                showlegend=showlegend,
+                ),
+                )
+
+    ################
+    # LAYOUT
+    ################
+    r_domain = min(np.round(1.25 - (len(units)*.15),2), 1)
+    fig.update_layout(
+        xaxis=dict(
+            domain=[0, r_domain]
         ),
-        secondary_y=False
+        yaxis=dict(
+            title="{}".format(titles[0]),
+        ),
     )
 
-    for col in df2.columns:
-        fig.add_trace(
-        go.Scatter(
-        name=col,
-        y=df2[col],
-        x=df2.index,
-        ),
-        secondary_y=True
-    )
+    positionl = .3
+    positionr = r_domain
+    for i in range(2,len(units)+1):
+        if i == 1:
+            positionl = 0.3
+            side = 'left'
+            anchor = 'x'
+            position = None
+        elif i == 2:
+            side = 'right'
+            positionl = positionl - .15
+            position = positionl
+            anchor = 'x'
+        else:
+            side= 'right'
+            positionr = positionr + .15
+            position = positionr
+            anchor = 'free'
 
-    fig.update_yaxes(title_text="<b>Energy Usage</b> (GJ)", secondary_y=False)
-    fig.update_yaxes(title_text="<b>CO2/Production</b> (MT/MT)",
-        secondary_y=True)
-
+        fig.update_layout(
+            {'yaxis{}'.format(i): {'anchor': anchor,
+                   'overlaying': 'y',
+                   'position': position,
+                   'side': side,
+                   'title': {'text': '{}'.format(titles[i-1])}
+                                  }
+        }
+        )
+    if len(items) == 1:
+        title="{}, By Site".format(items[0])
+    else:
+        title="By Site"
     fig.update_layout(dict(
-        barmode="stack",
-        title="OVERVIEW",
-        # clickmode='event+select',
+        title=title,
+        barmode="group",
     ),
     )
     return fig
@@ -256,6 +401,18 @@ app.layout = html.Div([
         multi=True,
         id='dropdown'
     ),
+    dcc.RadioItems(
+                id='sample',
+                options=[{'label': i, 'value': i} for i in ['Month', 'Year']],
+                value='Year',
+                labelStyle={'display': 'inline-block'}
+            ),
+    # dcc.RadioItems(
+    #             id='plot_type',
+    #             options=[{'label': i, 'value': i} for i in ['Bar', 'Scatter']],
+    #             value='Scatter',
+    #             labelStyle={'display': 'inline-block'}
+    #         ),
     # dcc.Graph(id='year_plot',
     #           figure=make_year_plot()
     #           ),
@@ -283,18 +440,20 @@ app.config.suppress_callback_exceptions = False
 
 @app.callback(
     Output('bar_plot', 'figure'),
-    [Input('time_plot', 'relayoutData')])
-def display_bar_data(relayoutData):
-    return make_bar_plot(relayoutData)
+    [Input('time_plot', 'relayoutData'),
+     Input('dropdown', 'value')])
+def display_bar_data(relayoutData, dropdown):
+    return make_bar_plot(relayoutData, dropdown)
 
 @app.callback(
     Output('time_plot', 'figure'),
     [Input('bar_plot', 'clickData'),
      Input('bar_plot', 'selectedData'),
-     Input('dropdown', 'value')]
+     Input('dropdown', 'value'),
+     Input('sample', 'value')]
 )
-def display_time_plot(barClickData, barSelectedData, dropdown):
-    return make_time_plot(barClickData, barSelectedData, dropdown)
+def display_time_plot(barClickData, barSelectedData, dropdown, sample):
+    return make_time_plot(barClickData, barSelectedData, dropdown, sample)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
